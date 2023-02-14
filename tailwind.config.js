d491aa8905e8458798e03d477d740d61d7e1350c
unknown <hernanvid123@gmail.com>
2023-02-14 16:14:00 -0500

    error image

--- tailwind.config.js
+++ tailwind.config.js
@@ -70,8 +70,8 @@ module.exports = {
         }
       },
       backgroundImage: {
-        'header': "url('./src/assets/hero.jpg')",
-        'header-sm': "url('./src/assets/desktop/hero.jpg')"
+        'header': "url('/src/assets/hero.jpg')",
+        'header-sm': "url('/src/assets/desktop/hero.jpg')"
       }
     },
   },
